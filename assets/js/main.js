c4e19c7e8b4d36483e17fd0de5c25665e62c6f12
Erik Salas <25646533+eriksalascode@users.noreply.github.com>
2022-04-17 14:55:51 -0500

    fix smooth scrolling functionality for all browsers

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,7 @@ sr.reveal(".contact__text", { interval: 200 });
 sr.reveal(".contact__input", { delay: 400 });
 sr.reveal(".contact__button", { delay: 600 });
 
-/* Fix header background color */
+/*===== FIX HEADER BACKGROUND COLOR =====*/
 let viewportWidth = window.innerWidth;
 console.log(viewportWidth);
 
@@ -137,7 +137,31 @@ if (viewportWidth < 768) {
   obs.observe(sectionHomeEl);
 }
 
-//get current year
+/*===== GET CURRENT YEAR =====*/
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+/*===== IMPLEMENT SMOOT SCROLLING IN ALL BROWSERS =====*/
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
